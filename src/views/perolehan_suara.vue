8a969fe098077aab5712d9c8cdca2c3354ad6252
Richal Akbar <richalakbarical@gmail.com>
2025-02-13 03:08:20 +0800

    Initial commit

---
+++ src/views/perolehan_suara.vue
@@ -0,0 +1,16 @@
+<template>
+  <navbar-component />
+  <div class="container-global">
+    <h1>Perolehan Suara</h1>
+  </div>
+</template>
+
+<script>
+import navbarComponent from "../components/navbarComponent.vue";
+export default {
+  name: "Suara",
+  components: {
+    navbarComponent,
+  },
+};
+</script>
